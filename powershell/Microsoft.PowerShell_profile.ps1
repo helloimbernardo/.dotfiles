566227efa845bd97d73b908d2f4967e382bae8af
Bernardo Oliveira <43864475+helloimbernardo@users.noreply.github.com>
2024-07-10 18:27:25 +0100

    added `nebuladownload` function to powershell

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -29,6 +29,35 @@ function Edit-Profile {
     vim $PROFILE
 }
 
+function nebuladownload {
+	<#
+	.SYNOPSIS
+		Downloads videos from Nebula with highest resolution available and all subtitles
+	.INPUTS
+		Video URL on nebula.tv (can be piped and accepts multiple piped values)
+	.OUTPUTS
+		Downloads video and subtitles (if available) on current directory
+	.EXAMPLE
+	        NebulaDownload "https://nebula.tv/video/example1"
+		This command downloads the video and all subtitles from the specified URL.
+	.EXAMPLE
+		"https://nebula.tv/video/example1", "https://nebula.tv/video/example2" | NebulaDownload
+		This command downloads videos and all subtitles from the specified URLs using the pipeline.
+	.NOTES
+		Ensure yt-dlp is installed and accessible in the system's PATH.
+	#>
+
+	[CmdletBinding()]
+    	param (
+		[Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]
+	        [string]$VideoUrl
+	)
+	process {
+		$command = "yt-dlp.exe --netrc --write-subs --sub-langs 'all' $VideoUrl"
+		Invoke-Expression $command
+	}
+}
+
 function spotify-player {
 	<#
 	.SYNOPSIS
--- scripts-windows/scripts.ps1
+++ scripts-windows/scripts.ps1
@@ -1,1 +1,2 @@
 Write-Output "wslcompact -> reduce space used up by WSL Virtual Disks"
+Write-Output "nebuladownload -> download video from nebula.tv"
